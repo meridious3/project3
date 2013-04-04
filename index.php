9efaddb624d0ae6064c7f133677becf75f8a7071
Will Douglas <willdouglas@Yggdrasil.Yggdrasil-domain>
2013-04-04 12:49:18 -0500

    The halma game is now working.

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<script src="js/board-localstorage.js"></script>
+	<script src="js/board.js"></script>
+</head>
+	<body>
+		<h1>Reversi</h1>
+		<p id="moves">Moves: <span id="movecount"></span></p>
+		<script>initGame(null,document.getElementById('movecount'));</script>
+	</body>
+</html>
