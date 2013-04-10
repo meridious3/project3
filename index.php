4a10a35876545481463b18b98ff91b1cb2248efc
meridious3 <bwm10a@acu.edu>
2013-04-10 11:51:07 -0500

    Added FB SDK for later integration

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?
+  // Remember to copy files from the SDK's src/ directory to a
+  // directory in your application on the server, such as php-sdk/
+  require 'facebook-php-sdk-master/src/facebook.php';
+
+  $config = array(
+    'appId'  => '627872940559966',
+    'secret' => 'b60c9ec277821046d62e733ff692f14f',
+  );
+
+  $facebook = new Facebook($config);
+  $user_id = $facebook->getUser();
+  
+  if(isset($_REQUEST["action"]))
+    $action = $_REQUEST["action"];
+  else
+    $action = "none";
+?>
 <html>
 <script>
 (function main() {
